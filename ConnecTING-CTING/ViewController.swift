d6a489ecab3e88af970c22d12ae4aac6870b6756
Sunhyeok Jung <sunhyeok.jung22@myhunter.cuny.edu>
2021-06-03 11:19:11 -0400

    Initial Commit

---
+++ ConnecTING-CTING/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConnecTING-CTING
+//
+//  Created by sunhyeok on 2021/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
